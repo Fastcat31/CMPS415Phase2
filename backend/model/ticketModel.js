2d4e159304a255d72bad816b01c06ec28a414ae6
Fastcat31 <jeremy.clay@selu.edu>
2023-04-20 20:13:40 -0500

    phase 2

---
+++ backend/model/ticketModel.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+const ticketFormat = new mongoose.Schema(
+  {
+    type: String,
+
+    subject: String,
+
+    description: String,
+
+    priority: String,
+
+    status: String,
+
+    recipient: String,
+
+    submitter: String,
+
+    assignee_id: [Number],
+
+    follower_ids: [Number],
+
+    tags: [String],
+  },
+
+  { timestamps: true }
+);
+
+let ticket = mongoose.model("Ticket", ticketFormat);
+
+module.exports = ticket;
